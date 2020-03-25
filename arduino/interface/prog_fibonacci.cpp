d3884c37d9d1fa046e627ba2bccb91b1beabbf4d
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2020-03-24 22:49:41 -0700

    Add real fibonacci to interface and put more data in foobar.

--- arduino/interface/prog_fibonacci.cpp
+++ arduino/interface/prog_fibonacci.cpp
@@ -1,9 +1,23 @@
 #include "prog_fibonacci.h"
 
-extern const byte num_fibonacci_program_bytes = 2;
+extern const byte num_fibonacci_program_bytes = 13;
 extern const byte fibonacci_program_bytes[] PROGMEM = {
-    0xFF, // SOME INSTRUCTION
-    0x12  // SOME INSTRUCTION
+    // @set_initial
+    0x39, // SET A #1
+    0x01, 
+    0x3A, // SET B #1
+    0x01,
+
+    // @fib_loop
+    0x08, // COPY A ACC
+    0xCE, // ADD B
+    0x24, // JUMP_IF_OVERFLOW_FLAG @set_initial
+    0x00, 
+    0x03, // COPY ACC C (to display)
+    0x11, // COPY B A
+    0x02, // COPY ACC B
+    0x3D, // JUMP @fib_loop
+    0x04
 };
 
 extern const byte num_fibonacci_data_bytes = 0;
