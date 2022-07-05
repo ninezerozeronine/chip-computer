47b7e2f85f32eb18ec97320055c3fcb29e7a34b1
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2022-07-04 22:35:37 -0700

    Add copy assembly tests, correct a few errors in other tests.

--- dev_helpers/sandbox.py
+++ dev_helpers/sandbox.py
@@ -2,14 +2,16 @@ import sys
 import os
 from pprint import pprint
 import textwrap
+import random
+from itertools import product
 
 sys.path.append(os.path.abspath("../src"))
 print(sys.path)
-from sixteen_bit_computer import rom, main
+from sixteen_bit_computer import main
 
 
 def sandbox():
-
+    pass
     # rom_path = r"E:\dev\8bit-74-series-computer\logisim\roms"
     # rom.write_logisim_roms(rom_path)
 
@@ -20,27 +22,27 @@ def sandbox():
     # print romdatas[3]
     # print romdatas[4]
 
-    romdatas = rom.get_rom()
-    print("done")
-    slices = rom.slice_rom(romdatas)
-    slice_index = 1
-    romdata_chunks = rom.chunker(slices[slice_index], 16)
-    count = 0
-    for sixteen_index, romdata_chunk in enumerate(romdata_chunks):
-        for line_index, romdata in enumerate(romdata_chunk):
-            print("{hex_sixteen_index:04X} {sixteen_index:03} {line_index:04} {count:05} {romdata}".format(
-                hex_sixteen_index=sixteen_index*16,
-                sixteen_index=sixteen_index,
-                line_index=line_index+1,
-                count=count,
-                romdata=romdata,
-            ))
-            count += 1
+    # romdatas = rom.get_rom()
+    # print("done")
+    # slices = rom.slice_rom(romdatas)
+    # slice_index = 1
+    # romdata_chunks = rom.chunker(slices[slice_index], 16)
+    # count = 0
+    # for sixteen_index, romdata_chunk in enumerate(romdata_chunks):
+    #     for line_index, romdata in enumerate(romdata_chunk):
+    #         print("{hex_sixteen_index:04X} {sixteen_index:03} {line_index:04} {count:05} {romdata}".format(
+    #             hex_sixteen_index=sixteen_index*16,
+    #             sixteen_index=sixteen_index,
+    #             line_index=line_index+1,
+    #             count=count,
+    #             romdata=romdata,
+    #         ))
+    #         count += 1
 
-    print(romdatas[128])
+    # print(romdatas[128])
 
-    logisim_string = rom.rom_slice_to_logisim_string(slices[slice_index])
-    print(logisim_string)
+    # logisim_string = rom.rom_slice_to_logisim_string(slices[slice_index])
+    # print(logisim_string)
 
     # romdatas = rom.get_defined_romdata()
     # full_rom = rom.populate_empty_addresses(romdatas)
@@ -107,6 +109,46 @@ def try_dedent():
     print(a)
     print(a.splitlines())
 
+def create_copy_tests():
+    """
+    &copy_<index>
+        SET SRC #random
+        SET ACC #random
+        COPY SRC DEST
+        JUMP_IF_EQ_ACC DEST &copy_<index + 1>
+        HALT
+
+
+    """
+    sources = ("ACC", "A", "B", "C", "SP")
+    destinations = ("ACC", "A", "B", "C", "SP")
+    lines = []
+    index = 0
+    for src, dest in product(sources, destinations):
+        if (src != dest):
+            num = random.randint(0, 65535)
+            if dest == "ACC":
+                lines.append("&copy_{index}".format(index=index))
+                lines.append("    SET {src} #{num}".format(src=src, num=num))
+                lines.append("    COPY {src} ACC".format(src=src))
+                lines.append("    JUMP_IF_ACC_EQ #{num} &copy_{index_plus_one}".format(num=num, index_plus_one=index+1))
+                lines.append("    HALT")
+                lines.append("")
+            else:    
+                lines.append("&copy_{index}".format(index=index))
+                if src != "ACC":
+                    lines.append("    SET {src} #{num}".format(src=src, num=num))
+                lines.append("    SET ACC #{num}".format(num=num))
+                lines.append("    COPY {src} {dest}".format(src=src, dest=dest))
+                lines.append("    JUMP_IF_ACC_EQ {dest} &copy_{index_plus_one}".format(dest=dest, index_plus_one=index+1))
+                lines.append("    HALT")
+                lines.append("")
+
+            index += 1
+
+    print("\n".join(lines))
+
+
 if __name__ == "__main__":
     # sandbox()
     # file_test()
@@ -114,4 +156,5 @@ if __name__ == "__main__":
     # dict_ref_test_2()
     # assemble_test()
     # main.create_roms(directory="./test_roms")
-    try_dedent()
+    # try_dedent()
+    create_copy_tests()
