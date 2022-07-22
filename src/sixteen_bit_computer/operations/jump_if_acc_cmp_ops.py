515e20fac7606b9995ca6cc4e78dbc727e3ab526
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2022-07-21 22:56:32 -0700

    Rename op modules with xs to more explicit names.

--- src/sixteen_bit_computer/operations/jump_if_acc_cmp_ops.py
+++ src/sixteen_bit_computer/operations/jump_if_acc_cmp_ops.py
@@ -8,23 +8,6 @@ Jumps to a location in memory based on the result of the
 comparison between the given module or constant and ACC.
 """
 
-
-
-# 1001     SET ACC #2                  | 1053 0x041D -                   109 0x006D
-#                                      | 1054 0x041E -                     2 0x0002 #2
-# 1002     JUMP_IF_ACC_LT A &jialt_1   | 1055 0x041F -                   132 0x0084
-#                                      | 1056 0x0420 -                  1058 0x0422 &jialt_1
-# 1003     HALT                        | 1057 0x0421 -                   186 0x00BA
-# 1004                                 |
-# 1005 &jialt_1                        |
-# 1006     SET B #123                  | 1058 0x0422 - &jialt_1          111 0x006
-
-# At 1057 instead of halting this jumped to 186
-
-
-
-
-
 from ..instruction_listings import get_instruction_index
 from ..data_structures import Word
 from ..instruction_components import (
