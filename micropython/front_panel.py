22d74fc87ab0ea3bb947765b67cc41a0ea68b7d2
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2022-08-28 22:22:16 -0700

    Progress on micropython interface and front panel.

--- micropython/front_panel.py
+++ micropython/front_panel.py
@@ -1,9 +1,15 @@
+import time
+
+from . import interface
+
+
 RUN = 100
 STOP = 101
 
 STEP = 200
-VARIABLE = 201
+FREQUENCY = 201
 CRYSTAL = 202
+
 
 class FrontPanel():
     """
@@ -15,84 +21,267 @@ class FrontPanel():
         Initialise the class.
         """
 
-        self._mode = STOP
+        self._run_mode = STOP
         self._clock_mode = STEP
+        self._frequency = 10000
 
     def run():
         """
         Put the computer into run mode.
         """
-        pass
+        if self._run_mode != RUN:
+            # Disable any output onto the buses from the interface
+            self._set_interface_bus_asserts(False)
+
+            # Allow CPU to assert onto the buses.
+            self._set_cpu_bus_asserts(True)
+
+            # Enable the clock on the CPU
+            interface.set_cpu_clock_input_enabled(True)
+
+            self._run_mode = RUN
 
     def stop():
         """
         Put the computer into stop mode.
         """
-        pass
+        if self._run_mode != STOP:
+            # Set the clock mode to STEP
+            self.set_clock_mode(STEP)
+
+            # Advance the clock so both clocks are low.
+            while not self._at_beginning_of_clock_cycle():
+                self.quarter_step()
+
+            # Disable the clock on the CPU
+            interface.set_cpu_clock_input_enabled(False)
+
+            # Disable the CPU from asserting onto the buses.
+            self._set_cpu_bus_asserts(False)
+
+            # Disable any output onto the buses from the interface
+            self._set_interface_bus_asserts(False)
+
+            self._run_mode = STOP
 
     def toggle_run_stop():
         """
         Toggle the computer between run and stop mode.
         """
-        pass
+        if self._run_mode == RUN:
+            self.stop()
+        if self._run_mode == STOP:
+            self.run()
 
     def reset():
         """
         Reset the computer.
         """
-        pass
+        interface.set_reset(True)
+        time.sleep_ms(1)
+        interface.set_reset(False)
 
     def quarter_step(self):
         """
         Advance the CPU by a quarter step.
         """
-        pass
+        if (self._clock_mode == STEP) and (self._run_mode == RUN):
+             self._send_clock_pulses(1)
 
     def half_step(self):
         """
         Advance the CPU by a half step.
         """
-        pass
+        if (self._clock_mode == STEP) and (self._run_mode == RUN):
+             self._send_clock_pulses(2)
 
     def full_step(self):
         """
         Advance the CPU by a full step.
         """
-        pass
+        if (self._clock_mode == STEP) and (self._run_mode == RUN):
+             self._send_clock_pulses(4)
 
     def set_clock_frequency(self, frequency):
         """
         Set the clock frequency of the CPU (in Hz)
         """
-        pass
 
-    def set_clock_mode(self, mode):
+        self._frequency = frequency
+
+        if (self._clock_mode == FREQUENCY)
+            if self._run_mode == RUN:
+                interface.set_cpu_clock_input_enabled(False)
+                interface.set_clock_pin_frequency(
+                    self._adjust_clock_frequency(self._frequency)
+                )
+                interface.set_cpu_clock_input_enabled(True)
+
+            if self._run_mode == STOP:
+                interface.set_clock_pin_frequency(
+                    self._adjust_clock_frequency(self._frequency)
+                )
+
+    def set_clock_mode(self, clock_mode):
         """
         Set the clock mode.
 
-        The available modes are STEP, VARIABLE, and CRYSTAL:
+        The available modes are STEP, FREQUENCY, and CRYSTAL:
 
          - STEP: The clock is advanced 1, 2, or 4 steps at a time.
-         - VARIABLE: The clock can be set to a desired frequency in Hz.
+         - FREQUENCY: The clock can be set to a desired frequency in Hz.
          - CRYSTAL: The speed of the clock is determined by the crystal
            in the front panel.
         """
-        pass
+        if clock_mode == self._clock_mode:
+            return
+
+        if self._run_mode == RUN:
+            interface.set_cpu_clock_input_enabled(False)
+
+        if clock_mode == STEP:
+            interface.set_clock_pin_static_state(False)
+            interface.set_clock_source(interface.MICROCONTROLLER)
+
+        if clock_mode == FREQUENCY:
+            interface.set_clock_pin_frequency(
+                self._adjust_clock_frequency(self._frequency)
+            )
+            interface.set_clock_source(interface.MICROCONTROLLER)
+
+        if clock_mode == CRYTSAL:
+            interface.set_clock_source(interface.CRYSTAL)
+
+        if self._run_mode == RUN:
+            interface.set_cpu_clock_input_enabled(True)
 
     def get_word(self, address):
         """
         Get a word from the device on the data bus at the given address.
         """
-        pass
+
+        word = 0
+
+        if self._run_mode == STOP:
+            interface.set_address(address)
+            interface.set_read_from_mem(True)
+            interface.set_write_to_mem(False)
+            interface.set_interface_address_assert(True)
+            interface.set_interface_read_write_mem_assert(True)
+            time.sleep_us(10)
+            word = interface.get_data()
+            interface.set_read_from_mem(False)
+            interface.set_write_to_mem(False)
+            interface.set_interface_address_assert(False)
+            interface.set_interface_read_write_mem_assert(False)
+
+        return word
 
     def set_word(self, address, word):
         """
         Set a word on the device on the data bus at the given address.
         """
-        pass
+        if self._run_mode == STOP:
+            interface.set_address(address)
+            interface.set_data(word)
+            interface.set_read_from_mem(False)
+            interface.set_write_to_mem(True)
+            interface.set_interface_address_assert(True)
+            interface.set_interface_data_assert(True)
+            interface.set_interface_read_write_mem_assert(True)
+            interface.set_interface_clock_assert(True)
+            time.sleep_us(10)
+            self._send_cpu_like_clock_cycle()
+            time.sleep_us(10)
+            interface.set_read_from_mem(False)
+            interface.set_write_to_mem(False)
+            interface.set_interface_address_assert(False)
+            interface.set_interface_data_assert(False)
+            interface.set_interface_read_write_mem_assert(False)
+            interface.set_interface_clock_assert(False)
 
     def set_words(self, addresses_and_words):
         """
         Set a number of addresses and words at once.
         """
-        pass
+        if self._run_mode == STOP:
+
+            interface.set_read_from_mem(False)
+            interface.set_write_to_mem(True)
+            interface.set_interface_address_assert(True)
+            interface.set_interface_data_assert(True)
+            interface.set_interface_read_write_mem_assert(True)
+            interface.set_interface_clock_assert(True)
+
+            for address, word in addresses_and_words:
+                interface.set_address(address)
+                interface.set_data(word)
+                time.sleep_us(10)
+                self._send_cpu_like_clock_cycle()
+                time.sleep_us(10)
+
+            interface.set_read_from_mem(False)
+            interface.set_write_to_mem(False)
+            interface.set_interface_address_assert(False)
+            interface.set_interface_data_assert(False)
+            interface.set_interface_read_write_mem_assert(False)
+            interface.set_interface_clock_assert(False)
+
+    def _at_beginning_of_clock_cycle(self):
+        """
+        Test whether the CPU is at the beginning of a clock cycle.
+
+        This happens when both the data and control clocks are low.
+        """
+
+        data_clock = interface.get_data_clock()
+        control_clock = interface.get_control_clock()
+        return not (data_clock or control_clock)
+
+    def _set_interface_bus_asserts(self, state):
+        """
+        Set whether the interface should assert onto the buses.
+        """
+        interface.set_interface_data_assert(state)
+        interface.set_interface_address_assert(state)
+        interface.set_interface_read_write_mem_assert(state)
+        interface.set_interface_clock_assert(state)
+
+    def _set_cpu_bus_asserts(self, state):
+        """
+        Set whether the CPU should assert onto the buses.
+        """
+        interface.set_cpu_data_assert(state)
+        interface.set_cpu_address_assert(state)
+        interface.set_cpu_read_write_mem_assert(state)
+        interface.set_cpu_clock_assert(state)
+
+    def _send_clock_pulses(self, num_pulses):
+        """
+        Send the specified number of clock pulses.
+        """
+        for _ in range(num_pulses):
+            interface.set_clock_pin_static_state(True)
+            time.sleep_ms(1)
+            interface.set_clock_pin_static_state(False)
+
+    def _adjust_clock_frequency(self, frequency):
+        """
+        Adjust the clock frequency to account for the clock processing
+        in the CPU.
+        """
+
+        return frequency * 4
+
+    def _send_cpu_like_clock_cycle(self):
+        """
+        Create a clock cycle comparable to that genertaed by the CPU.
+        """
+        interface.set_control_clock(True)
+        time.sleep_us(10)
+        interface.set_data_clock(True)
+        time.sleep_us(10)
+        interface.set_control_clock(False)
+        time.sleep_us(10)
+        interface.set_data_clock(False)
+        time.sleep_us(10)
--- micropython/interface.py
+++ micropython/interface.py
@@ -2,8 +2,33 @@
 The low level hardware interface to the computer.
 """
 
+from machine import Pin, PWM, Timer
+
 MICROCONTROLLER = 100
-CRYSYAL = 101
+CRYSTAL = 101
+
+_STATIC = 200
+_TIMER = 201
+_PWM = 202
+
+_DATA_BUS = 300
+_ADDRESS_BUS = 301
+_CONTROL_CLOCK_BUS = 302
+_DATA_CLOCK_BUS = 303
+_WTM_BUS = 304
+_RFM_BUS = 305
+
+_CPU_CLOCK_PIN_NO = 25
+
+_BUS_IN_INPUT_REG_CLOCK_PIN_NO = 1
+_BUS_IN_PARALLEL_LOAD_PIN_NO = 2
+_BUS_IN_SHIFT_CLOCK_PIN_NO = 4
+_BUS_IN_SERIAL_READ_PIN_NO = 5
+
+_BUS_OUT_SERIAL_IN_PIN_NO = 6
+_BUS_OUT_SHIFT_CLOCK_PIN_N0 = 7
+_BUS_OUT_OUTPUT_REG_CLOCK_PIN_NO = 9
+
 
 class Interface():
     """
@@ -23,140 +48,382 @@ class Interface():
         self._cpu_address_assert = False
 
         self._read_from_mem = False
-        self._interface_read_from_mem_assert = False
-        self._cpu_read_from_mem_assert = False
-
         self._write_to_mem = False
-        self._interface_write_to_mem_assert = False
-        self._cpu_write_to_mem_assert = False
+        self._interface_read_write_mem_assert = False
+        self._cpu_read_write_mem_assert = False
 
         self._data_clock = False
         self._control_clock = False
-        self._interface_assert_clock_bus = False
-        self._cpu_assert_clock_bus = False
+        self._interface_clock_bus_assert = False
+        self._cpu_clock_bus_assert = False
 
         self._cpu_clock_input_enabled = False
 
+        self._clock_mode = _STATIC
+        self._clock_pin = Pin(_CPU_CLOCK_PIN_NO, mode=Pin.OUT, value=False)
+        self._pwm = PWM(self._clock_pin)
+        self._pwm.deinit()
+        self._clock_pin = Pin(_CPU_CLOCK_PIN_NO, mode=Pin.OUT, value=False)
+        self._timer = Timer()
+
+        # Pins to control shift registers that read the state of the buses
+        self._bus_in_input_reg_clock_pin = Pin(
+            _BUS_IN_INPUT_REG_CLOCK_PIN_NO, mode=Pin.OUT, value=False
+        )
+        self._bus_in_parallel_load_pin = Pin(
+            _BUS_IN_PARALLEL_LOAD_PIN_NO, mode=Pin.OUT, value=True
+        )
+        self._bus_in_shift_clock_pin = Pin(
+            _BUS_IN_SHIFT_CLOCK_PIN_NO, mode=Pin.OUT, value=False
+        )
+        self._bus_in_serial_read_pin = Pin(
+            _BUS_IN_SERIAL_READ_PIN_NO, mode=Pin.IN, pull=Pin.PULL_DOWN
+        )
+
+        # Pins to control shift registers that assert onto the buses
+        self._bus_out_serial_in_pin = Pin(
+            _BUS_OUT_SERIAL_IN_PIN_NO, mode=Pin.OUT, value=False
+        )
+        self._bus_out_shift_clock_pin = Pin(
+            _BUS_OUT_SHIFT_CLOCK_PIN_N0, mode=Pin.OUT, value=False
+        )
+        self._bus_out_output_reg_clock_pin = Pin(
+            _BUS_OUT_OUTPUT_REG_CLOCK_PIN_NO, mode=Pin.OUT, value=False
+        )
+
         self._clock_source = MICROCONTROLLER
+
+        self._reset = False
+
+        self._address_assert = False
+        self._data_assert = False
+        self._ctl_data_clks_assert = False
+        self._wtm_rfm_assert = False
+
+        self._shift_out()
 
     def set_data(self, data):
         """
         Set the data to be output to the data bus.
         """
-        pass
+
+        # Clamp data to 16 bit value
+        if address < 0:
+            address = 0
+        if address > 65536:
+            address = 65536
+
+        self._data = data
+        self._shift_out()
 
     def get_data(self):
         """
         Get the data currently on the data bus.
         """
-        pass
+        bus_states = self._shift_in()
+        return bus_states[_DATA_BUS]
 
     def set_interface_data_assert(self, state):
         """
         Set whether the interface should assert onto the data bus.
         """
-        pass
+
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._cpu_data_assert and state):
+            self._interface_data_assert = state
+            self._shift_out()
 
     def set_cpu_data_assert(self, state):
         """
         Set whether the CPU should assert onto the data bus.
         """
-        pass
 
-    def set_address(self):
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._interface_data_assert and state):
+            self._cpu_data_assert = state
+            self._shift_out()
+
+    def set_address(self, address):
         """
         Set the address to put on the address bus.
         """
-        pass
+
+        # Clamp address to 16 bit value
+        if address < 0:
+            address = 0
+        if address > 65536:
+            address = 65536
+
+        self._address = address
+        self._shift_out()
 
     def get_address(self):
         """
         Get the address currently on the address bus.
         """
-        pass
+        bus_states = self._shift_in()
+        return bus_states[_ADDRESS_BUS]
 
     def set_interface_address_assert(self, state):
         """
         Set whether the interface should assert onto the address bus.
         """
-        pass
+
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._cpu_address_assert and state):
+            self._interface_address_assert = state
+            self._shift_out()
 
     def set_cpu_address_assert(self, state):
         """
         Set whether the CPU should assert onto the address bus.
         """
-        pass
+
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._interface_address_assert and state):
+            self._cpu_address_assert = state
+            self._shift_out()
 
     def get_read_from_mem(self):
         """
         Get the state of the read from memory bus.
         """
-        pass
-
-    def set_interface_read_from_mem_assert(self, state):
-        """
-        Set whether the interface should assert onto the read from
-        memory bus.
-        """
-        pass
-
-    def set_cpu_read_from_mem_assert(self, state):
-        """
-        Set whether the CPU should assert onto the read from memory bus.
-        """
-        pass
+        bus_states = self._shift_in()
+        return bus_states[_RFM_BUS]
 
     def get_write_to_mem(self):
         """
         Get the state of the write to memory bus.
         """
-        pass
+        bus_states = self._shift_in()
+        return bus_states[_WTM_BUS]
 
-    def set_interface_write_to_mem_assert(self, state):
+    def set_interface_read_write_mem_assert(self, state):
         """
-        Set whether the interface should assert onto the write to
-        memory bus.
+        Set whether the interface should assert onto the write to and
+        read from memory buses.
         """
-        pass
 
-    def set_cpu_write_to_mem_assert(self, state):
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._cpu_read_write_mem_assert and state):
+            self._interface_read_write_mem_assert = state
+            self._shift_out()
+
+    def set_cpu_read_write_mem_assert(self, state):
         """
-        Set whether the CPU should assert onto the write to memory bus.
+        Set whether the CPU should assert onto the write to and read
+        from memory buses.
         """
-        pass
+
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._interface_read_write_mem_assert and state):
+            self._cpu_read_write_mem_assert = state
+            self._shift_out()
 
     def get_data_clock(self):
         """
         Get the state of the data clock bus.
         """
-        pass
+        bus_states = self._shift_in()
+        return bus_states[_DATA_CLOCK_BUS]
+
+    def set_data_clock(self, state):
+        """
+        Set the state to assert onto the data clock bus.
+        """
+        self._data_clock = state
+        self._shift_out()
 
     def get_control_clock(self):
         """
         Get the state of the control clock bus.
         """
-        pass
+        bus_states = self._shift_in()
+        return bus_states[_CONTROL_CLOCK_BUS]
 
-    def set_interface_clock_assert(self, state):
+    def set_control_clock(self, state):
+        """
+        Set the state to assert onto the control clock bus.
+        """
+        self._control_clock = state
+        self._shift_out()
+
+    def set_interface_clock_bus_assert(self, state):
         """
         Set whether the interface should assert onto the clock buses.
         """
-        pass
 
-    def set_cpu_clock_assert(self, state):
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._cpu_clock_bus_assert and state):
+            self._interface_clock_bus_assert = state
+            self._shift_out()
+
+    def set_cpu_clock_bus_assert(self, state):
         """
         Set whether the CPU should assert onto the clock buses.
         """
-        pass
+
+        # Protect from CPU and interface  asserting onto the bus
+        if not (self._interface_clock_bus_assert and state):
+            self._cpu_clock_bus_assert = state
+            self._shift_out()
 
     def set_cpu_clock_input_enabled(self, state):
         """
         Set whether the CPU clock input is enabled or not.
         """
-        pass
+        self._cpu_clock_input_enabled = state
+        self._shift_out()
 
     def set_clock_source(self, source):
         """
         Set the source for the clock.
         """
-        pass
+        if source in (MICROCONTROLLER, CRYSTAL):
+            self._source = source
+        self._shift_out()
+
+
+    def set_reset(self, state):
+        """
+        Set the state of the reset line.
+        """
+        self._reset = state
+        self._shift_out()
+
+    def set_clock_pin_static_state(self, state):
+        """
+        Set the clock pin to a static state
+        """
+
+        # Disable the timer if is't currently active
+        if self._clock_mode == _TIMER:
+            self._timer.deinit()
+
+        # Disable the PWM if it's currently active and reset the pin
+        # object
+        if self._clock_mode == _PWM:
+            self._pwm.deinit()
+            self._clock_pin = Pin(
+                _CPU_CLOCK_PIN_NO, mode=Pin.OUT, value=False
+            )
+
+        # Set the pin to the desired state
+        self._clock_pin.value(state)
+        self._clock_mode = _STATIC
+
+    def set_clock_pin_frequency(self, frequency):
+        """
+        Set the clock pin to a certain frequency.
+        """
+
+        # If the frequency is low
+        if frequency < 10:
+            # Disable the PWM if it's active and reset the clock pin.
+            if self._clock_mode == _PWM:
+                self._pwm.deinit()
+                self._clock_pin = Pin(
+                    _CPU_CLOCK_PIN_NO, mode=Pin.OUT, value=False
+                )
+
+            # We were either in static or timer mode so just
+            # re-init the timer.
+            self._timer.init(
+                freq=frequency * 2,
+                mode=Timer.PERIODIC,
+                callback=self._timer_callback
+            )
+
+            self._clock_mode = _TIMER
+
+        # Otherwise the frequency is high
+        else:
+            if self._clock_mode == _TIMER:
+                self._timer.deinit()
+                self._pwm = PWM(self._clock_pin)
+
+            if self._clock_mode == _STATIC:
+                self._pwm = PWM(self._clock_pin)
+
+            self._pwm.duty_u16(32768)
+            self._pwm.freq(frequency)
+
+            self._clock_mode = _PWM
+
+    def _shift_in(self):
+        """
+        Shift in the state of the buses
+        """
+
+        return {}
+
+    def _shift_out(self):
+        """
+        Shift out the control signals.
+
+        The control signals on each shift register are:
+
+         - SR0: Address 0-7
+         - SR1: Address 8-15
+         - SR2: Data 0-7
+         - SR3: Data 8-15
+         - SR4-0: Control clock
+         - SR4-1: Data clock
+         - SR4-2: Write to memory
+         - SR4-3: Read from memory
+         - SR4-4: Clock source
+         - SR4-5: Reset
+         - SR4-6: <blank>
+         - SR4-7: <blank>
+         - SR5-0: Interface address bus assert
+         - SR5-1: Interface data bus assert
+         - SR5-2: Interface clock bus asset
+         - SR5-3: Interface RFM/WTM bus assert
+         - SR5-4: CPU address bus assert
+         - SR5-5: CPU data bus assert
+         - SR5-6: CPU clock bus asset
+         - SR5-7: CPU RFM/WTM bus assert
+        """
+
+        # Convert the logical values here to the hardware values i.e.,
+        # account for active low inputs on some of the chips.
+
+        # Shift Register 5
+        self._shift_bit_out(self._cpu_read_write_mem_assert)
+        self._shift_bit_out(self._cpu_clock_bus_assert)
+        self._shift_bit_out(self._cpu_data_assert)
+        self._shift_bit_out(self._cpu_address_assert)
+        self._shift_bit_out(self._interface_read_write_mem_assert)
+        self._shift_bit_out(self._interface_clock_bus_assert)
+        self._shift_bit_out(not self._interface_data_assert)
+        self._shift_bit_out(not self._interface_address_assert)
+
+        # Shift Register 4
+        self._shift_bit_out(False)
+        self._shift_bit_out(False)
+        self._shift_bit_out(self._reset)
+        if self._clock_source == MICROCONTROLLER:
+            self._shift_bit_out(False)
+        else:
+            self._shift_bit_out(True)
+        self._shift_bit_out(self._read_from_mem)
+        self._shift_bit_out(self._write_to_mem)
+        self._shift_bit_out(self._data_clock)
+        self._shift_bit_out(self._control_clock)
+
+
+    def _shift_bit_out(self, bit):
+        """
+        Shift out a single bit.
+
+        This doesn't latch it into the output register though.
+        """
+        self._bus_out_serial_in_pin.value(bit)
+        self._bus_out_shift_clock_pin.value(True)
+        self._bus_out_shift_clock_pin.value(False)
+
+    def _timer_callback(self, timer):
+        """
+        Timer callback for low frequency clocks
+        """
+        self._clock_pin.toggle()
