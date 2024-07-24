5510e570719763b55f53d448954da6afa96ccc42
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2024-07-23 21:11:23 -0700

    Update jump if (n)eq zero ops.

--- src/sixteen_bit_computer/operations/jump_if_eq_neq_zero_ops.py
+++ src/sixteen_bit_computer/operations/jump_if_eq_neq_zero_ops.py
@@ -13,8 +13,6 @@ from ..instruction_components import (
     A,
     B,
     C,
-    PC,
-    SP,
     CONST
 )
 from .. import number_utils
@@ -30,12 +28,10 @@ _SUPPORTED_SIGNATURES = (
     (JUMP_IF_EQ_ZERO, A, CONST),
     (JUMP_IF_EQ_ZERO, B, CONST),
     (JUMP_IF_EQ_ZERO, C, CONST),
-    (JUMP_IF_EQ_ZERO, SP, CONST),
     (JUMP_IF_NEQ_ZERO, ACC, CONST),
     (JUMP_IF_NEQ_ZERO, A, CONST),
     (JUMP_IF_NEQ_ZERO, B, CONST),
     (JUMP_IF_NEQ_ZERO, C, CONST),
-    (JUMP_IF_NEQ_ZERO, SP, CONST),
 )
 
 
@@ -62,10 +58,10 @@ def generate_machinecode(signature, const_tokens):
 
 def generate_microcode_templates():
     """
-    Generate microcode for the JUMP_IF_EQ_ZERO operation.
+    Generate microcode for the JUMP_IF_(N)EQ_ZERO operations.
 
     Returns:
-        list(DataTemplate): DataTemplates for all the JUMP_IF_EQ_ZERO microcode.
+        list(DataTemplate): DataTemplates for all the JUMP_IF_(N)EQ_ZERO microcode.
     """
 
     data_templates = []
@@ -90,31 +86,11 @@ def generate_microcode_templates():
         })
 
         # If the zero flag was set, perform the jump.
-        #
-        # PC is currently pointing at the location in memory that holds the
-        # location to jump to (this was the second instruction word).
-        # Set the MAR with the value of PC so it can then be loaded from memory.
         step = 1
         if signature[0] == JUMP_IF_EQ_ZERO:
             flags = [FLAGS["ZERO"]["HIGH"]]
         elif signature[0] == JUMP_IF_NEQ_ZERO:
             flags = [FLAGS["ZERO"]["LOW"]]
-        module_controls = [
-            MODULE_CONTROL["PC"]["OUT"],
-            MODULE_CONTROL["MAR"]["IN"],
-        ]
-        microcode_defs.append({
-            "step": step,
-            "flags": flags,
-            "module_controls": module_controls,
-        })
-
-        # Next step of the jump.
-        #
-        # Set PC to the value of the second instruction word and reset the
-        # microcode step to fetch the next instruction from that location.
-        step = 2
-        flags = [FLAGS["ANY"]]
         module_controls = [
             MODULE_CONTROL["MEM"]["READ_FROM"],
             MODULE_CONTROL["PC"]["IN"],
@@ -125,6 +101,7 @@ def generate_microcode_templates():
             "flags": flags,
             "module_controls": module_controls,
         })
+
 
         # If the zero flag wasn't set, increment the program counter to move past
         # the constant that would have been jumped to and reset the step counter.
