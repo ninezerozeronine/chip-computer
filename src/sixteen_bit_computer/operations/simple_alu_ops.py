124e69af4c8c2e318b86e7c3e4fd125b44bed9d4
Andy Palmer <25123779+ninezerozeronine@users.noreply.github.com>
2024-07-24 20:18:42 -0700

    Update simple ALU ops.

--- src/sixteen_bit_computer/operations/simple_alu_ops.py
+++ src/sixteen_bit_computer/operations/simple_alu_ops.py
@@ -137,15 +137,31 @@ def _generate_control_steps(signature):
 
     elif signature[1] == CONST:
         step_0 = [
-            MODULE_CONTROL["PC"]["OUT"],
+            MODULE_CONTROL["MEM"]["READ_FROM"],
+            MODULE_CONTROL["ALU"]["STORE_RESULT"],
+            MODULE_CONTROL["ALU"]["STORE_FLAGS"],
+            MODULE_CONTROL["PC"]["COUNT"],
+        ]
+        step_0.extend(ALU_CONTROL_FLAGS[_alu_opcode_to_alu_mode(signature[0])])
+
+        step_1 = [
+            MODULE_CONTROL["ALU"]["OUT"],
+            MODULE_CONTROL["ACC"]["IN"],
+        ]
+
+        control_steps = [step_0, step_1]
+
+    elif signature[1] == M_CONST:
+        step_0 = [
+            MODULE_CONTROL["MEM"]["READ_FROM"],
             MODULE_CONTROL["MAR"]["IN"],
+            MODULE_CONTROL["PC"]["COUNT"]
         ]
 
         step_1 = [
             MODULE_CONTROL["MEM"]["READ_FROM"],
             MODULE_CONTROL["ALU"]["STORE_RESULT"],
             MODULE_CONTROL["ALU"]["STORE_FLAGS"],
-            MODULE_CONTROL["PC"]["COUNT"],
         ]
         step_1.extend(ALU_CONTROL_FLAGS[_alu_opcode_to_alu_mode(signature[0])])
 
@@ -155,51 +171,6 @@ def _generate_control_steps(signature):
         ]
 
         control_steps = [step_0, step_1, step_2]
-
-    elif signature[1] == M_CONST:
-        step_0 = [
-            MODULE_CONTROL["PC"]["OUT"],
-            MODULE_CONTROL["MAR"]["IN"],
-        ]
-
-        # This is a little sketchy, on the rising clock edge we're
-        # simultaneously:
-        # - Reading a value from memory
-        # - Changing the address in memory we're looking at
-        # step_1 = [
-        #     MODULE_CONTROL["MEM"]["READ_FROM"],
-        #     MODULE_CONTROL["MAR"]["IN"],
-        #     MODULE_CONTROL["PC"]["COUNT"],
-        # ]
-
-        # Temporarily storing the memory address in the ALU for safety
-        # reasons...
-        step_1 = [
-            MODULE_CONTROL["MEM"]["READ_FROM"],
-            MODULE_CONTROL["ALU"]["A_IS_BUS"],
-            MODULE_CONTROL["ALU"]["STORE_RESULT"],
-            MODULE_CONTROL["PC"]["COUNT"],
-        ]
-        step_1.extend(ALU_CONTROL_FLAGS["A"])
-
-        step_2 = [
-            MODULE_CONTROL["ALU"]["OUT"],
-            MODULE_CONTROL["MAR"]["IN"],
-        ]
-
-        step_3 = [
-            MODULE_CONTROL["MEM"]["READ_FROM"],
-            MODULE_CONTROL["ALU"]["STORE_RESULT"],
-            MODULE_CONTROL["ALU"]["STORE_FLAGS"],
-        ]
-        step_3.extend(ALU_CONTROL_FLAGS[_alu_opcode_to_alu_mode(signature[0])])
-
-        step_4 = [
-            MODULE_CONTROL["ALU"]["OUT"],
-            MODULE_CONTROL["ACC"]["IN"],
-        ]
-
-        control_steps = [step_0, step_1, step_2, step_3, step_4]
 
     return control_steps
 
